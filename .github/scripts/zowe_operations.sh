7cf7437eaffc096ae77508e84fba5c21e0c9a01e
LM Santos <luizmrdsantos@gmail.com>
2025-05-13 15:16:46 -0700

    Addedd ZOWE_HOST in yml

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -5,7 +5,7 @@ LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 # Check if directory exists, create if it doesn't
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
 echo "Directory does not exist. Creating it..."
-zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck" # --host $ZOWE_HOST
+zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck" --host $ZOWE_HOST
 else
 echo "Directory already exists."
 fi
