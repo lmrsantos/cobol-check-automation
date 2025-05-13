719039ec3e2357a44c1bde160779788850c10632
LM Santos <luizmrdsantos@gmail.com>
2025-05-13 16:37:17 -0700

    test if sh was called.

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -3,7 +3,7 @@
 # Convert username to lowercase
 echo "I AM IN"
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
-echo "lowercase username:" $LOWERCASE_USERNAME
+echo $ZOWE_USERNAME $ZOWE_PASSWORD $ZOWE_HOST
 # Check if directory exists, create if it doesn't
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
 echo "Directory does not exist. Creating it..."
