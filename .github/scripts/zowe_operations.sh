ba21c42638e3e1ed9cba3f69cb63dacf5d5daa4a
LM Santos <luizmrdsantos@gmail.com>
2025-05-15 18:31:01 -0700

    changed local variables

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # zowe_operations.sh
 # Convert username to lowercase
-LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
+LOWERCASE_USERNAME=$(echo "$Z_USER" | tr '[:upper:]' '[:lower:]')
 # Check if directory exists, create if it doesn't
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"  --host $Z_HOST --password $Z_PSW --user $Z_USER &>/dev/null; then
-echo "Directory does not exist. Creating it..."
-zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --host $Z_HOST
-else
-echo "Directory already exists."
-fi
-# Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --host $Z_HOST --recursive --binary-files "cobol-check-0.2.9.jar"
-# Verify upload
-echo "Verifying upload..."
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+echo "host:" ${Z_HOST} "PSW: " $Z_PSW "USERNAME: "${ secrets.ZOWE_USERNAME }
+# if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"  --host ${Z_HOST} --password $Z_PSW --user $Z_USER &>/dev/null; then
+# echo "Directory does not exist. Creating it..."
+# zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --host $Z_HOST
+# else
+# echo "Directory already exists."
+# fi
+# # Upload files
+# zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --host $Z_HOST --recursive --binary-files "cobol-check-0.2.9.jar"
+# # Verify upload
+# echo "Verifying upload..."
+# zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
