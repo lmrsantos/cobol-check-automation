59a0e971b5874680e93600337024c2676b202e1f
LM Santos <luizmrdsantos@gmail.com>
2025-05-19 15:07:41 -0700

    Added $USER.Data and some comments in the scripts and yaml.

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # mainframe_operations.sh
 # Set up environment
-# export PATH=$PATH:/usr/lpp/java/J8.0_64/bin
-# export JAVA_HOME=/usr/lpp/java/J8.0_64
-# export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
+export PATH=$PATH:/usr/lpp/java/J8.0_64/bin
+export JAVA_HOME=/usr/lpp/java/J8.0_64
+export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
 # Check Java availability
 java -version
 # Set ZOWE_USERNAME
