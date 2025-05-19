1feb8d3e604fd4d73caa700d6cd4f9ad79b0b9bc
LM Santos <luizmrdsantos@gmail.com>
2025-05-19 11:35:08 -0700

    Changes in shell and yaml

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -5,12 +5,12 @@ LOWERCASE_USERNAME=$(echo "$USER" | tr '[:upper:]' '[:lower:]')
 # Check if directory exists, create if it doesn't
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host $HOST --password $PSW --user $USER &>/dev/null; then
 echo "Directory does not exist. Creating it..."
-zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --host $HOST --password $PSW --user $USER 
+zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --host $HOST --password $PSW --user $USER --port $PORT
 else
 echo "Directory already exists."
 fi
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --host $HOST --password $PSW --user $USER --recursive --binary-files "cobol-check-0.2.9.jar"
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --host $HOST --password $PSW --user $USER --port $PORT --recursive --binary-files "cobol-check-0.2.9.jar"
 # Verify upload
 echo "Verifying upload..."
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host $HOST --password $PSW --user $USER 
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host $HOST --password $PSW --user $USER --port $PORT
